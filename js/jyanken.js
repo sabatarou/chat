354f3257c318b34f0530f3538def96f410d593fc
Ayatakamaru <buddatyan@gmail.com>
2017-07-27 23:31:19 +0900

    jyanken.js readme.txt 追記 js/timer.js ranking.php削除　#009

--- js/jyanken.js
+++ js/jyanken.js
@@ -1,10 +1,22 @@
-$(function()
+const start_time=9999;
+let now_time=start_time;
+function countup()
+  {
+		if(now_time>0)
+		{
+			now_time--;
+			if()
+			{
+		    jQuery( '#time' ) .text(parseInt(now_time/100)+":"+0+(now_time-(parseInt(now_time/100)*100)));
+			}
+			else
+			{
+	      jQuery( '#time' ) .text(parseInt(now_time/100)+":"+(now_time-(parseInt(now_time/100)*100)));
+			}
+	  }
+  }
+function time()
 {
-	$('#time').click
-  (function()
-   {
-	  jQuery('#time').text("okokokok");
-	 }
-  );
- }
-);
+		setInterval("countup(now_time)", 10);
+}
+time();
--- readme.txt
+++ readme.txt
@@ -3,3 +3,7 @@ http://homutuku.com/beginner/210.html
 https://saruwakakun.com/html-css/reference/h-design
 jquery sample
 https://webkikaku.co.jp/blog/webdesign/jquery_start/
+jquery
+http://jquery.com/
+font
+http://www.fontspace.com/category/cyber
